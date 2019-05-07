52efca9d6c99a96a6b03af5f5b86824846c64133
Dmitriy Myakonkikh <dimabdb@yandex.ru>
2019-05-07 08:28:01 +0700

    Format according to codestyle and add .travis.yml

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include "board_print_plain.h"
+#include <stdio.h>
 extern char desk[8][8];
 void printboard()
 {
-int i, j;
-for (i = 0; i < 8; i++) {
+    int i, j;
+    for (i = 0; i < 8; i++) {
         for (j = 0; j < 8; j++)
             printf("%c", desk[i][j]);
         printf("\n");
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include"board.h"
+#include "board.h"
 #include "board_print_plain.h"
+#include <stdio.h>
 
 char input[7];
 int X1, X2, Y1, Y2;
@@ -13,22 +13,20 @@ char desk[8][8] = {{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
 
-
 int main()
 {
-  printboard();
-  
-  for(;;)
-{
-scan(1);
-move();
-printboard();
+    printboard();
 
-scan(2);
-move();
-printboard();
-break;
-}
+    for (;;) {
+        scan(1);
+        move();
+        printboard();
 
-return 0;
+        scan(2);
+        move();
+        printboard();
+        break;
+    }
+
+    return 0;
 }
